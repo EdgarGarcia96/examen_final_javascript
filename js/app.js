5eccb9d7d69f0d87725e2980e9cf8be2e69182e0
Edgar <edgar960212@gmail.com>
2018-03-16 15:27:33 -0500

    metodos funcionales de operaciones

--- js/app.js
+++ js/app.js
@@ -1,16 +1,30 @@
 var calculadora = {
-
+	num_uno:0,
+	num_dos:0,
+	num_dos_total:0,
+	total:0,
+	operador:'',
+	operador_total:'',
 	iniciar:function(){
 		var tecla = document.getElementsByClassName("tecla");
 		for(var i in tecla){
 			tecla[i].onclick = this.asignarValor;
+			tecla[i].onmousedown = this.presionar;
 		}
+	},
+	presionar:function(sel){
+
+		console.log(sel.toElement);
+		sel.toElement.style='transform: scale(0.9);';
 	},
 	asignarValor:function(sel){
 		var tecla = sel.toElement.id;
-
+		console.log(sel.toElement);
+		sel.toElement.style='transform: ;';
+		//sel.transform.scale='(0.8)';
+		//
+		console.log(tecla);
 		if (!isNaN(tecla)){
-			console.log('aca');
 			var num_ant = document.getElementById('display').innerHTML;
 			num_ant = num_ant!='0' ? num_ant += tecla : tecla;
 			calculadora.textoPantalla(num_ant);
@@ -37,33 +51,111 @@ var calculadora = {
 				calculadora.textoPantalla(num_ant);
 				break;
 			case 'mas':
+				calculadora.sumar();
 				break;
 			case 'menos':
+				calculadora.restar();
 				break;
 			case 'por':
+				calculadora.multiplicar();
 				break;
-			case 'dividor':
+			case 'dividido':
+				calculadora.dividir();
+				break;
+			case 'igual':
+					var total = calculadora.operar();
+					calculadora.textoPantalla(total);
+					calculadora.operador='';
 				break;
 		}
 	},
 	textoPantalla:function(texto){
-		if(texto.includes("-") && texto.length<10){
+		if(texto.includes("-")){
+			// && texto.length<10
+			texto = texto.substr(0,9);
 			document.getElementById('display').innerHTML=texto;
-		}else if(texto.length<9){
+		}else{
+			// if(texto.length<9)
+			texto = texto.substr(0,8);
 			document.getElementById('display').innerHTML=texto;
 		}
 	},
 	sumar:function(){
-
+		if(calculadora.operador==''){
+			calculadora.operador='+';
+			calculadora.num_uno = parseFloat(document.getElementById('display').innerHTML);
+			calculadora.textoPantalla('');
+		}
+		calculadora.num_dos_total = '';
+		calculadora.operador_total = '';
 	},
 	restar:function(){
-		console.log('hola');
+		if(calculadora.operador==''){
+			calculadora.operador='-';
+			calculadora.num_uno = parseFloat(document.getElementById('display').innerHTML);
+			calculadora.textoPantalla('');
+		}
+		calculadora.num_dos_total = '';
+		calculadora.operador_total = '';
 	},
 	multiplicar:function(){
-		
+		if(calculadora.operador==''){
+			calculadora.operador='*';
+			calculadora.num_uno = parseFloat(document.getElementById('display').innerHTML);
+			calculadora.textoPantalla('');
+		}
+		calculadora.num_dos_total = '';
+		calculadora.operador_total = '';
 	},
 	dividir:function(){
-		
+		if(calculadora.operador==''){
+			calculadora.operador='/';
+			calculadora.num_uno = parseFloat(document.getElementById('display').innerHTML);
+			calculadora.textoPantalla('');
+		}
+		calculadora.num_dos_total = '';
+		calculadora.operador_total = '';
+	},
+	operar:function(){
+		calculadora.num_dos = parseFloat(document.getElementById('display').innerHTML);
+
+		if(calculadora.operador_total==''){
+			calculadora.num_dos_total = calculadora.num_dos;
+			calculadora.operador_total = calculadora.operador;
+			switch (calculadora.operador) {
+				case '-':
+					calculadora.total = calculadora.num_uno - calculadora.num_dos;
+					break;
+				case '+':
+					calculadora.total = calculadora.num_uno + calculadora.num_dos;
+					break;
+				case '*':
+					calculadora.total = calculadora.num_uno * calculadora.num_dos;
+					break;
+				case '/':
+					calculadora.total = calculadora.num_uno / calculadora.num_dos;
+					break;
+			}
+		}else{
+			//calculadora.operador=calculadora.operador_total;
+			switch (calculadora.operador_total) {
+				case '-':
+					calculadora.total = calculadora.total - calculadora.num_dos_total;
+					break;
+				case '+':
+					calculadora.total = calculadora.total + calculadora.num_dos_total;
+					break;
+				case '*':
+					calculadora.total = calculadora.total * calculadora.num_dos_total;
+					break;
+				case '/':
+					calculadora.total = calculadora.total / calculadora.num_dos_total;
+					break;
+			}
+		}
+		var total = calculadora.total.toString();
+
+		return total;
 	}
 };
 
